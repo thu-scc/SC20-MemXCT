2a78982e27ac949420c151b8fce8d66d8d3f75ca
Jiajie Chen <c@jia.je>
2020-11-03 19:45:08 +0800

    Rewrite cpu script to use openmp threads

--- ReproducibilityChallenge/run/scripts/manual_run.cpu.sh
+++ ReproducibilityChallenge/run/scripts/manual_run.cpu.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
+module load icc impi
 
-#NPROC=$1
-#DATA=$2
+#NTASK=$1
+#NTHREAD=$2
+#DATA=$3
 
 # TILE SIZE
-#SPATSIZE=$3
-#SPECSIZE=$3
+#SPATSIZE=$4
+#SPECSIZE=$4
 #
 # BLOCK SIZE
-#PROJBLOCK=$4
-#BACKBLOCK=$4
+#PROJBLOCK=$5
+#BACKBLOCK=$5
 #
 # BUFFER SIZE
-#PROJBUFF=$5
-#BACKBUFF=$5
+#PROJBUFF=$6
+#BACKBUFF=$6
 
 
-source para.sh $2 $3 $3 $4 $4 $5 $5
+source para.sh $2 $3 $4 $4 $5 $5 $6 $6
 
 HOSTNAME=$(hostname)
 mkdir -p ../output/original/$HOSTNAME/cpu
-FILE="../output/original/$HOSTNAME/cpu/$1.$2.$3.$3.$4.$4.$5.$5.out"
+FILE="../output/original/$HOSTNAME/cpu/$1.$2.$3.$4.$4.$5.$5.$6.$6.out"
 
-mpirun -np $1 ../../compile/cpu-build/memxct.cpu > ${FILE}
+#mpirun -np $1 ../../compile/cpu-build/memxct.cpu > ${FILE}
+srun -n $1 ../../compile/cpu-build/memxct.cpu > ${FILE}
 
 tot_bw=$(grep -E 'av: \w+.\w+' -o < $FILE | awk '{print $2}')
 av_gflops=$(grep -E 'avGFLOPS: \w+.\w+' -o < $FILE | awk '{print $2}')
@@ -31,11 +34,11 @@ prep_time=$(grep -E 'PREPROCESSING TIME: \w+.\w+[+-]\w+' -o $FILE | awk '{print 
 tot_time=$(grep -E 'Total Time: \w+.\w+[+-]\w+' -o < $FILE | awk '{print $3}')
 
 #echo $tot_gflops,$tot_bw,$prep_time,$tot_time
-echo Use $2 dataset by $1 proc
-echo proc, tile_size, block_size, buffer_size, tot_gflops, tot_bw, tot_time
-echo $1,$3,$4,$5,$tot_gflops,$tot_bw,$tot_time
+echo Use $3 dataset by $1 tasks, $2 threads
+echo task, thread, tile_size, block_size, buffer_size, tot_gflops, tot_bw, tot_time
+echo $1,$2,$4,$5,$6,$tot_gflops,$tot_bw,$tot_time
 echo tot_gflops $tot_gflops
 echo av_gflops $av_gflops
-#echo tot_bw $tot_bw GB/s
-#echo prep_time $prep_time sec
-#echo tot_time $tot_time sec
+echo tot_bw $tot_bw GB/s
+echo prep_time $prep_time sec
+echo tot_time $tot_time sec
