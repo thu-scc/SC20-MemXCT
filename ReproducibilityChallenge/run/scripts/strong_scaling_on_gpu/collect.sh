4579eda87b072578682d65ae8b866566a6aae9d0
monellz <monell.zhong@gmail.com>
2020-11-09 03:20:18 +0000

    Strong scaling for gpu(p100)

--- ReproducibilityChallenge/run/scripts/strong_scaling_on_gpu/collect.sh
+++ ReproducibilityChallenge/run/scripts/strong_scaling_on_gpu/collect.sh
@@ -4,7 +4,7 @@
 set -x
 
 # azure
-curl --connect-timeout 1s -H 'Metadata: true' http://169.254.169.254/metadata/instance?api-version=2020-06-01 2>/dev/null >metadata.json
+curl  -H 'Metadata: true' http://169.254.169.254/metadata/instance?api-version=2020-06-01 2>/dev/null >metadata.json
 echo 'VM size:' $(jq -r .compute.vmSize metadata.json)
 echo 'VM name:' $(jq -r .compute.name metadata.json)
 echo 'VM location:' $(jq -r .compute.location metadata.json)
@@ -12,30 +12,16 @@ echo 'Resource group:' $(jq -r .compute.resourceGroupName metadata.json)
 echo 'Private ip address:' $(jq -r .network.interface[0].ipv4.ipAddress[0].privateIpAddress metadata.json)
 echo 'Public ip address:' $(jq -r .network.interface[0].ipv4.ipAddress[0].publicIpAddress metadata.json)
 echo 'CPU model': $(lscpu | sed -nr '/Model name/ s/.*:\s*(.*)/\1/p')
-echo "CPU:"
 
 # compilers
 which gcc
 gcc --version
-which icc
-icc --version
 which nvcc
 nvcc --version
 
 # cpu
 lscpu || cat /proc/cpuinfo
-cat /sys/devices/cpu/caps/pmu_name
 gcc -march=native -Q --help=target | grep march
-
-# ipmi
-echo "Board:"
-ipmitool fru print 0 | grep "Board Product"
-ipmitool fru print 0 | grep "Board Serial"
-ipmitool fru print 0 | grep "Board Part Number"
-echo "Product:"
-ipmitool fru print 0 | grep "Product Name"
-ipmitool fru print 0 | grep "Product Part Number"
-ipmitool fru print 0 | grep "Product Serial"
 
 # env
 unset SSH_CLIENT
@@ -57,7 +43,6 @@ nvidia-smi
 
 # hpc package manager
 spack find
-module list
 
 # loaded package
 spack find --loaded -l
